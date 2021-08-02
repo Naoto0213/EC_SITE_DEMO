1d249c93bbe274f6f347107cf4024fe545abd88f
githubアカウントのユーザー <m.naotomachida@gmail.com>
2021-08-02 19:11:15 +0900

    Authフォルダを追加しました

--- src/router/Router.js
+++ src/router/Router.js
@@ -6,8 +6,8 @@ import SignUp from "../pages/SignUp";
 const Router = () => {
   return (
     <Switch>
-      <Route exact path="/login" component={Login} />
       <Route exact path="(/)" component={Home} />
+      <Route exact path="/login" component={Login} />
       <Route exact path="/signup" component={SignUp} />
     </Switch>
   );
